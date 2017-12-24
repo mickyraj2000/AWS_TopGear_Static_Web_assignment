831abbb4e59a80249260af6fd133dc6cf44d57ff
Kaushik Roy <kaushik.roy@wipro.com>
2017-12-24 10:36:08 +0530

    Commiting TopGear AWS Project assignment

---
+++ README.md
@@ -0,0 +1,1 @@
+http://topgear205072-env.us-east-2.elasticbeanstalk.com/
---
+++ src/main/java/com/testkart/App.java
@@ -0,0 +1,13 @@
+package com.testkart;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Static web AWS project!" );
+    }
+}
